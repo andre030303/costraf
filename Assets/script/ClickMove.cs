6f9bc1cf9de14da1814c27358d6960744618dd78
andre030303 <109279860+andre030303@users.noreply.github.com>
2022-09-12 22:15:35 +0300

    да

---
+++ Assets/script/ClickMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickMove : MonoBehaviour
+{
+   public float speed;
+    private Vector3 TPosition;
+    private bool isMoving = false;
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            TPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            TPosition.z = transform.position.z;
+            isMoving = true;
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            isMoving = false;
+        }
+        if (isMoving)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, TPosition);
+            transform.position = Vector3.MoveTowards(transform.position, TPosition, speed * Time.fixedDeltaTime);
+            if (transform.position == TPosition)
+                isMoving = false;
+        }
+    }
+}
+
