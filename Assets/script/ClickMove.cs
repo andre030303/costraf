3a7643dacfcd659b9d93221a8e5c703648c492ac
andre030303 <109279860+andre030303@users.noreply.github.com>
2022-09-13 15:22:59 +0300

    dvig 2.0

--- Assets/script/ClickMove.cs
+++ Assets/script/ClickMove.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class ClickMove : MonoBehaviour
 {
-   public float speed;
+    public float speed;
+    public float rotation;
     private Vector3 TPosition;
     private bool isMoving = false;
     void FixedUpdate()
-    {
-        if (Input.GetMouseButton(0))
+    {   
+        if (Input.GetMouseButton(0))//запуск програмы если нажата левая кноака мишы
         {
-            TPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            TPosition.z = transform.position.z;
-            isMoving = true;
+            TPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);//определение кардинат курсора
+            TPosition.z = transform.position.z;//замена каодинаты Z курсора на кардинату Z обекта, нам не надо менять глубину потомучто 2D
+            isMoving = false ;//запрешаем запуск движения по кардинатом курсора
+            float rotz= Mathf.Atan2(TPosition.y,TPosition.x)*Mathf.Rad2Deg; //математические фокусы для вычесления необходимых градусов
+            transform.rotation = Quaternion.Euler(0f,0f,rotz+rotation);//поворот обекта на градусы из переменой rotz с прибавлением нежной скорости 
         }
-        else if (Input.GetMouseButton(1))
+        if (false==Input.GetMouseButton(0))//запуск програмы после отжатия левой кнопки мыши
         {
-            isMoving = false;
+            isMoving = true;//разрешаем запуск движения по кардинатом курсора
         }
         if (isMoving)
         {
-            transform.rotation = Quaternion.LookRotation(Vector3.forward, TPosition);
-            transform.position = Vector3.MoveTowards(transform.position, TPosition, speed * Time.fixedDeltaTime);
-            if (transform.position == TPosition)
-                isMoving = false;
+            transform.position = Vector3.MoveTowards(transform.position, TPosition, speed * Time.fixedDeltaTime);//двежения на сохранёные каординаты мышки
+            if (transform.position == TPosition)//запуск програмы если сохранёные каординаты мышки совподают с кардинатоми обекта 
+                isMoving = false;//запрешаем запуск движения по кардинатом курсора
         }
     }
 }
