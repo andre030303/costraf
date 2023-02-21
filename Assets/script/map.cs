6385454e87ef3b78901772e16a1c8a00c3008476
andre030303 <kyl.ind@yandex.ru>
2023-02-21 17:00:05 +0300

    level 2

--- Assets/script/map.cs
+++ Assets/script/map.cs
@@ -9,22 +9,34 @@ public class map : MonoBehaviour
 
     public GameObject strelka;
 
+    public GameObject Text;
+
     public void star1()
-    {   
+    {
         SceneManager.LoadScene(2);
         strelka.SetActive(true);
-    }      
+    }
+
+    public void star2()
+    {
+        SceneManager.LoadScene(3);
+        strelka.SetActive(true);
+    }
 
     void Update()
     {
-        if (!(GameObject.FindGameObjectWithTag("Enemy")) && Input.GetKey(KeyCode.F))
+        if (!(GameObject.FindGameObjectWithTag("Enemy")))
+        {
+            Text.SetActive(true);
+        }
+        if (Input.GetKey(KeyCode.M) && !(GameObject.FindGameObjectWithTag("Enemy")))
         {
             mape.SetActive(true);
+            Text.SetActive(false);
         }
         else
         {
             mape.SetActive(false);
         }
-        
     }
 }
